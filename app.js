77195f806a07e3678e7ff097c1a7d3eaf9a67f7f
Jade <jade.elise.a@gmail.com>
2019-12-14 14:19:22 -0800

    bonus assignment complete

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 function onReady(){
-  let toDos = [];
-  let id = 0;
+  let toDos = JSON.parse(localStorage.getItem('toDos')) || [];
+
+
+function getNextIdOnLoad() {
+  let next = 0
+  for(let i = 0; i < toDos.length; i++) {
+    if(toDos[i].id > next) next = toDos[i].id
+  }
+  if(next > 0) return next+1
+  else return next;
+}
+  let id = getNextIdOnLoad()
   const addToDoForm = document.getElementById('addToDoForm');
   function createNewToDo(){
     const newToDoText = document.getElementById('newToDoText');
@@ -20,9 +30,10 @@ function onReady(){
     toDoList.textContent = '';
     toDos.forEach(function(toDo){
       const newLi = document.createElement('li');
-      const checkbox = document.createElement('input');
+      let checkbox = document.createElement('input');
       const deleteBtn = document.createElement('button');
       checkbox.type="checkbox";
+      checkbox.checked = toDo.complete
       newLi.textContent = toDo.title;
       deleteBtn.textContent="Delete!";
       toDoList.appendChild(newLi);
@@ -35,17 +46,32 @@ function onReady(){
         })
         renderTheUI();
       });
+      checkbox.addEventListener('change', function(){
+        let item = toDos.find(i  => i.id === toDo.id)
+        item.complete = this.checked
+        localStorage.setItem('toDos',JSON.stringify(toDos));
+      })
+
+        /* local storage */
+      localStorage.setItem('toDos',JSON.stringify(toDos));
+      const data = JSON.parse(localStorage.getItem('toDos'))
+      data.forEach(function(toDo){
+        createNewToDo(toDo)
+          })
 
     });
   }
 
-
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     createNewToDo();
+    localStorage.setItem('toDo', JSON.stringify(toDos[id]))
   });
+
+
   renderTheUI();
 }
+
 
 window.onload = function(){
   onReady();
